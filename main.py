1e6af9e795081d27663fa46de26e9bbba6fcc978
Dirk Oberste-Berghaus <mcpdob@hotmail.com>
2026-07-11 14:11:06 +0200

    v0.2 Live Shotgun event discovery

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from asyncio import events
+
 from rich.console import Console
 from rich.panel import Panel
 
 from config.settings import APP_NAME, APP_VERSION
 from database.database import Database
-from collectors.residentadvisor import ResidentAdvisorCollector
-
+# from collectors.residentadvisor import ResidentAdvisorCollector
+from collectors.shotgun import ShotgunCollector
 console = Console()
 
 
@@ -20,12 +22,16 @@ def startup():
     db = Database()
     db.create_tables()
 
-    collector = ResidentAdvisorCollector()
-
+   # collector = ResidentAdvisorCollector()
+    collector = ShotgunCollector()
     events = collector.collect()
 
-    console.print(f"\nTotal events: {len(events)}")
+    console.print(f"\nTotal events: {len(events)}\n")
 
+    for number, event in enumerate(events, start=1):
+
+        console.print(f"[bold cyan]{number}.[/bold cyan] {event['text']}")
+        console.print(f"   {event['href']}\n")
     db.close()
 
 
